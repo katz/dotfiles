2253d429b4ce3ac1938d818ea0089317fe691c7a
Katz Sakai <sakai@onflow.jp>
2026-02-27 18:45:08 +0900

    fix: add HOME/bin to PATH after chezmoi install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ GITHUB_USER="katz"
 # chezmoi がなければインストール
 if ! command -v chezmoi >/dev/null 2>&1; then
   sh -c "$(curl -fsLS get.chezmoi.io)"
+  export PATH="${HOME}/bin:${PATH}"
 fi
 
 # dotfiles を適用
